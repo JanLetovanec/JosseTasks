8b71dec43b929d6d8bc38bb6b9a09691a0626f65
JanLetovanec <janko.let@gmail.com>
2022-08-14 01:22:02 +0100

    Added some NotNUll flags

--- src/natasha/Vector3.java
+++ src/natasha/Vector3.java
@@ -1,34 +1,45 @@
 package natasha;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Vector3 {
-    public static Vector3 fromXYZ(double x, double y, double z) {
+
+    //TODO: Pass the tests.
+    // More handhold-y instructions:
+    // - get some `class` basics, like what are `members` and `objects`
+    // - get the constructor working and then the `fromXYZ` method
+    // - might be worth implementing `magnitude` and `subtract` first, since tests rely on those
+
+    // Given x,y,z give me back YOUR representation of the vector
+    // Note that you do not have to use Euclid-3D representation
+    public static @NotNull Vector3 fromXYZ(double x, double y, double z) {
         return null;
     }
 
     // Return a new vector that is the sum of 'this' and 'other'
     // Do not mutate either
-    public Vector3 add(Vector3 other) {
+    public @NotNull Vector3 add(@NotNull Vector3 other) {
         return null;
     }
 
     // Subtract two vectors (in the same manner)
-    public Vector3 subtract(Vector3 other) {
+    public @NotNull Vector3 subtract(@NotNull Vector3 other) {
         return null;
     }
 
     // Return a vector that is this vector scaled by scalar
     // Do not mutate
-    public Vector3 scale(double scalar) {
+    public @NotNull Vector3 scale(double scalar) {
         return null;
     }
 
     // Dot product of two vectors
-    public double dot(Vector3 other) {
+    public double dot(@NotNull Vector3 other) {
         return 0f;
     }
 
     // Cross product of two vectors
-    public Vector3 cross(Vector3 other) {
+    public @NotNull Vector3 cross(@NotNull Vector3 other) {
         return null;
     }
 
@@ -51,19 +62,20 @@ public class Vector3 {
 
     // Return a normalised vector
     // I.e. same direction, but unit length
+    // Not testing what you are doing on zero vector
     public Vector3 normalised() {
         return null;
     }
 
     // Calculate mirror-like reflection
     // I.e. given a normal to surface, calculate 'bounce'
+    // Should retain magnitude
     public Vector3 reflectIn(Vector3 normal) {
         return null;
     }
 
     // Creates a random vector inside the unit sphere
     public static Vector3 randomInsideUnitSphere() {
-
         double r = Math.random();
         double theta = Math.random() * Math.PI;
         double phi = Math.random() * Math.PI * 2;
